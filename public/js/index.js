f9365f56888fd48df8932cadf5db387944f53556
Heradocles <heradocles@gmail.com>
2020-10-07 18:55:14 -0600

    Alerts implementation

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ import { login, logout } from "./login";
 import { displayMap } from "./mapBox";
 import { updateSettings } from "./updateSettings";
 import { bookTour } from "./stripe";
+import { showAlert } from "./alerts";
 
 // DOM ELEMENTS
 const mapBox = document.getElementById("map");
@@ -63,3 +64,7 @@ if (bookBtn)
     const { tourId } = e.target.dataset;
     bookTour(tourId);
   });
+
+const alertMessage = document.querySelector("body").dataset.alert;
+
+if (alert) showAlert("Success", alertMessage, 6);
